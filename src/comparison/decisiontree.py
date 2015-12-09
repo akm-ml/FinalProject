1a292ab2461a5d6e74c8aa20ad8c5cca67ce3d57
matteocam <matteo.campanelli@gmail.com>
2015-12-08 19:05:43 -0500

    Added time logging code

--- src/comparison/decisiontree.py
+++ src/comparison/decisiontree.py
@@ -1,5 +1,6 @@
 from classifierutils import MLClassifier, MLData
 from sklearn import tree
+import time
 
 class DecTreeClassifier(MLClassifier):
     def train(self, params, trainingData):
@@ -8,26 +9,49 @@ class DecTreeClassifier(MLClassifier):
     def predict(self, testData):
         return self.clf.predict(testData.X)
 
+
+def logOnFile(fn, accuracy, trainTime, testTime):
+    # Log result
+    f = open(fn, 'w')
+    f.write("Accuracy: " + str(accuracy) + '\n')
+    f.write("Training time: " + str(trainTime) + '\n')
+    f.write("Testing time: " + str(testTime) + '\n')
+    f.close()
+
+
 def trainOnAllData():
     trainData = MLData('pr_data12')
     testData =  MLData('pr_data3')
-    
+
+    startTime = time.time()
     t = DecTreeClassifier()
     t.train({}, trainData)
+    trainTimeOffset = time.time()
+    trainTime = trainTimeOffset - startTime
+    
+    perf =  t.getPerformance(testData)
+    testTime = time.time() - trainTimeOffset
 
-    # Log result
-    f = open('accuracy-dec-tree', 'w')
-    f.write(str(t.getPerformance(testData)) + '\n')
-    f.close()
+    logOnFile("mini-test-dec-tree", perf, trainTime, testTime)
 
 def miniTest():
     trainData = MLData('miniTrainData')
     testData =  MLData('miniTestData')
-    print
+    
+
+    startTime = time.time()
     t = DecTreeClassifier()
     t.train({}, trainData)
-    print t.getPerformance(testData)
+    trainTimeOffset = time.time()
+    trainTime = trainTimeOffset - startTime
+    
+    perf =  t.getPerformance(testData)
+    testTime = time.time() - trainTimeOffset
+
+    logOnFile("mini-test-dec-tree", perf, trainTime, testTime)
+
 
 
 if __name__ == '__main__':
     trainOnAllData()
+    #miniTest()
