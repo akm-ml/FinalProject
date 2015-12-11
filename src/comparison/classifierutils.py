dd02338d214bf3e66bf6a1af10c8f734636f6ad0
matteocam <matteo.campanelli@gmail.com>
2015-12-11 16:00:12 -0500

    Abstracted experimenting and time measuring functions. Refactored

--- src/comparison/classifierutils.py
+++ src/comparison/classifierutils.py
@@ -2,12 +2,13 @@
 
 import numpy as np
 from sklearn.metrics import accuracy_score
+import time
 
 class MLData:
     def __init__(self, fn):
         self.X = []
         self.Y = []
-        
+
         # Load from file fn
         f = open(fn)
 
@@ -23,8 +24,8 @@ class MLData:
 
 # Abstract class for classifiers
 class MLClassifier:
-    # For each MLClassifier (in the subclasses) we define the (static) range of 
-    # possible parameters 
+    # For each MLClassifier (in the subclasses) we define the (static) range of
+    # possible parameters
     def __init__(self):
         pass
 
@@ -32,12 +33,16 @@ class MLClassifier:
         pass
     def getPerformance(self, testData):
         predictedY = self.predict(testData)
+        #print "Predicted:"
+        #print predictedY[:40]
+        #print "Original:"
+        #print testData.Y[:40]
         return accuracy_score(testData.Y, predictedY)
-    
+
     def train(self, params, trainingData):
         """ Given a dictionary of params it trains the ML accordingly"""
         pass
-            
+
 # Test all hyperparameters in range and returns best
 class Learner:
     def __init__(self, classifierClass, trainData, devData, testData, logFile):
@@ -47,7 +52,7 @@ class Learner:
         self.testData = testData
         self.bestParams = None
         self.logFile = logFile
-        
+
     def allPossibleParams(self):
         pass
     def findBestHyperParams(self):
@@ -56,16 +61,16 @@ class Learner:
         for params in self.allPossibleParams():
             # Log stuff about the new simulator here
             # TODO
-            
+
             sim = self.classifierClass()
             # XXX: Log time for training?
             sim.train(params)
-            
+
             performance = sim.getPerformance(devData)
             if performance > bestPerformance:
                 bestPerformace = performance
                 bestParams = params
-        
+
         # save best params
         self.bestParams = bestParams
         # Log about the best params/performance
@@ -73,11 +78,45 @@ class Learner:
     def go(self, testData):
         # Do the training with the test data (maybe we can save it from before?)
         # and test it on the test data
-        
+
         # return performance
         pass
-        
-        
-        
+
+
+def logOnFile(fn, accuracy, trainTime, testTime):
+    # Log result
+    f = open(fn, 'w')
+    f.write("Accuracy: " + str(accuracy) + '\n')
+    f.write("Training time: " + str(trainTime) + '\n')
+    f.write("Testing time: " + str(testTime) + '\n')
+    f.close()
+
+
+def measureTimeExecuting(f):
+  startTime = time.time()
+  res = f()
+  elapsedTime = time.time() - startTime
+  return (res, elapsedTime)
+
+def getAccuracyOn(trainDataFile, testDataFile, logFn, classifierClass):
+  trainData = MLData(trainDataFile)
+  testData = MLData(testDataFile)
+
+  t = classifierClass()
+
+  _, trainTime = measureTimeExecuting(lambda: t.train({}, trainData))
+  perf, testTime = measureTimeExecuting(lambda: t.getPerformance(testData))
+  logOnFile(logFn, perf, trainTime, testTime)
+
+
+def mkTestFun(trainDataFile, testDataFile):
+  return lambda logFn, classifierClass: getAccuracyOn(trainDataFile, testDataFile, logFn, classifierClass)
+
+
+
+miniTest = mkTestFun("miniTrainData", "miniTestData")
+globalTest = mkTestFun("pr_data12", "pr_data3")
+
+
 if __name__ == '__main__':
     print "Hello, this is a ML program!"
--- src/comparison/decisiontree.py
+++ src/comparison/decisiontree.py
@@ -1,6 +1,6 @@
-from classifierutils import MLClassifier, MLData
+from classifierutils import MLClassifier, MLData, miniTest, globalTest
 from sklearn import tree
-import time
+
 
 class DecTreeClassifier(MLClassifier):
     def train(self, params, trainingData):
@@ -10,48 +10,7 @@ class DecTreeClassifier(MLClassifier):
         return self.clf.predict(testData.X)
 
 
-def logOnFile(fn, accuracy, trainTime, testTime):
-    # Log result
-    f = open(fn, 'w')
-    f.write("Accuracy: " + str(accuracy) + '\n')
-    f.write("Training time: " + str(trainTime) + '\n')
-    f.write("Testing time: " + str(testTime) + '\n')
-    f.close()
-
-
-def trainOnAllData():
-    trainData = MLData('pr_data12')
-    testData =  MLData('pr_data3')
-
-    startTime = time.time()
-    t = DecTreeClassifier()
-    t.train({}, trainData)
-    trainTimeOffset = time.time()
-    trainTime = trainTimeOffset - startTime
-    
-    perf =  t.getPerformance(testData)
-    testTime = time.time() - trainTimeOffset
-
-    logOnFile("decision-tree-time-accuracy", perf, trainTime, testTime)
-
-def miniTest():
-    trainData = MLData('miniTrainData')
-    testData =  MLData('miniTestData')
-    
-
-    startTime = time.time()
-    t = DecTreeClassifier()
-    t.train({}, trainData)
-    trainTimeOffset = time.time()
-    trainTime = trainTimeOffset - startTime
-    
-    perf =  t.getPerformance(testData)
-    testTime = time.time() - trainTimeOffset
-
-    logOnFile("mini-test-dec-tree", perf, trainTime, testTime)
-
-
-
 if __name__ == '__main__':
-    trainOnAllData()
-    #miniTest()
+
+    miniTest("mini-test-dec-tree", DecTreeClassifier)
+    #globalTest("decision-tree-time-accuracy", DecTreeClassifier)
