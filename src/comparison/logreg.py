443952a08830496c2a7beb07135af83abae2f3f3
matteocam <matteo.campanelli@gmail.com>
2015-12-11 16:10:52 -0500

    Completed LogReg

--- src/comparison/logreg.py
+++ src/comparison/logreg.py
@@ -1,2 +1,13 @@
 from classifierutils import MLClassifier, MLData, miniTest, globalTest
 from sklearn.linear_model import LogisticRegression
+
+class LogRegClassifier(MLClassifier):
+    def train(self, params, trainingData):
+        self.clf = LogisticRegression()
+        self.clf = self.clf.fit(trainingData.X, trainingData.Y)
+    def predict(self, testData):
+        return self.clf.predict(testData.X)
+
+if __name__ == '__main__':
+    #miniTest("mini-test-logreg", LogRegClassifier)
+    globalTest("logreg-time-accuracy", LogRegClassifier)
